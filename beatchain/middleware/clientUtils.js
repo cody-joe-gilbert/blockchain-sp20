265ebd3ae95c4977e4c3dd70b9b80ccd8c5be26d
Cody Gilbert <codyjgilbert@gmail.com>
2020-04-03 10:55:20 -0400

    Editing/debugging of network middleware.

--- beatchain/middleware/clientUtils.js
+++ beatchain/middleware/clientUtils.js
@@ -269,9 +269,9 @@ function getUserMember(adminUser, adminPassword, client, userOrg, username) {
 }
 
 function getAdmin(client, userOrg) {
-	var keyPath = path.join(__dirname, util.format(Constants.networkLocation + '/crypto-config/peerOrganizations/%s.trade.com/users/Admin@%s.trade.com/msp/keystore', userOrg, userOrg));
+	var keyPath = path.join(__dirname, util.format(Constants.networkLocation + '/crypto-config/peerOrganizations/%s.beatchain.com/users/Admin@%s.beatchain.com/msp/keystore', userOrg, userOrg));
 	var keyPEM = Buffer.from(readAllFiles(keyPath)[0]).toString();
-	var certPath = path.join(__dirname, util.format(Constants.networkLocation + '/crypto-config/peerOrganizations/%s.trade.com/users/Admin@%s.trade.com/msp/signcerts', userOrg, userOrg));
+	var certPath = path.join(__dirname, util.format(Constants.networkLocation + '/crypto-config/peerOrganizations/%s.beatchain.com/users/Admin@%s.beatchain.com/msp/signcerts', userOrg, userOrg));
 	var certPEM = readAllFiles(certPath)[0];
 
 	var cryptoSuite = Client.newCryptoSuite();
@@ -296,9 +296,9 @@ function getOrdererMSPId() {
 }
 
 function getOrdererAdmin(client) {
-	var keyPath = path.join(__dirname, Constants.networkLocation + '/crypto-config/ordererOrganizations/trade.com/users/Admin@trade.com/msp/keystore');
+	var keyPath = path.join(__dirname, Constants.networkLocation + '/crypto-config/ordererOrganizations/beatchain.com/users/Admin@beatchain.com/msp/keystore');
 	var keyPEM = Buffer.from(readAllFiles(keyPath)[0]).toString();
-	var certPath = path.join(__dirname, Constants.networkLocation + '/crypto-config/ordererOrganizations/trade.com/users/Admin@trade.com/msp/signcerts');
+	var certPath = path.join(__dirname, Constants.networkLocation + '/crypto-config/ordererOrganizations/beatchain.com/users/Admin@beatchain.com/msp/signcerts');
 	var certPEM = readAllFiles(certPath)[0];
 
 	return Promise.resolve(client.createUser({

6c5c9cd581f9d49a105be0a354b886d9e01a2729
Cody Gilbert <codyjgilbert@gmail.com>
2020-04-13 11:31:57 -0400

    Editing/debugging of network middleware.

--- beatchain/middleware/constants.js
+++ beatchain/middleware/constants.js
@@ -49,7 +49,7 @@ var ACCEPT_ALL = {
 	}
 };
 
-var chaincodeLocation = '../chaincode';
+var chaincodeLocation = '../../beatchain';
 
 var networkId = 'beatchain';
 
@@ -64,7 +64,7 @@ var CREATOR_ORG = 'creatororg';
 var CUSTOMER_ORG = 'customerorg';
 
 var CHANNEL_NAME = 'fullchannel';
-var CHAINCODE_PATH = '.';
+var CHAINCODE_PATH = 'chaincode';
 var CHAINCODE_ID = 'beatchain_alpha';
 var CHAINCODE_VERSION = 'v0';
 

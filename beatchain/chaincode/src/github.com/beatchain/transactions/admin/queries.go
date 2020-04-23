e9d1c62c19d9e179b2348104d18b316177998e07
Arunachalam Muthupalaniappan <arun.ninja@outlook.com>
2020-04-23 18:14:23 -0400

    added listAppCustomers

--- beatchain/chaincode/src/github.com/beatchain/transactions/admin/queries.go
+++ beatchain/chaincode/src/github.com/beatchain/transactions/admin/queries.go
@@ -72,7 +72,7 @@ func ListBankAccounts(stub shim.ChaincodeStubInterface, transaction *utils.Trans
 }
 
 
-func ListCustomers(stub shim.ChaincodeStubInterface, transaction *utils.Transaction) pb.Response {
+func ListAllCustomers(stub shim.ChaincodeStubInterface, transaction *utils.Transaction) pb.Response {
 	/*
 		Lists all of the customer records and their details from the ledger
 
@@ -142,3 +142,87 @@ func ListCustomers(stub shim.ChaincodeStubInterface, transaction *utils.Transact
 	resultMsg := strings.Join(jsonOutput, "\n")
 	return shim.Success([]byte(resultMsg))
 }
+
+func ListAppCustomers(stub shim.ChaincodeStubInterface, transaction *utils.Transaction) pb.Response {
+	/*
+		Lists all of the customer records and their details from the ledger for a particular app dev eg: spotify
+
+		Args:
+			transaction: Creator's transaction info, AppdevID
+
+	*/
+	var currentCustomerRecord *utils.CustomerRecord
+
+	var jsonOutput []string
+	var err error
+	var keysIterator shim.StateQueryIteratorInterface
+
+
+	// Validate an ID is given
+	if !transaction.TestMode && transaction.CreatorId == "" {
+		return shim.Error(fmt.Sprintf("calling user ID not found"))
+	}
+	// Validate no other args are specified
+	if len(transaction.Args) != 1 {
+		return shim.Error(fmt.Sprintf("ListCustomers takes 1 argument : {AppdevId}"))
+	}
+
+	appDevId := txn.Args[0]
+	// check for valid AppDev
+	_, err = utils.GetAppDevRecord(stub, appDevId)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// Create an iterator for fetching keys
+	keysIterator, err = stub.GetStateByPartialCompositeKey("object~id", []string{utils.CUSTOMER_RECORD_KEY_PREFIX})
+	if err != nil {
+		fmt.Print("Key iterator error: ")
+		return shim.Error(err.Error())
+	}
+	defer keysIterator.Close()
+
+	// Loop through keys and print account balances
+	for keysIterator.HasNext() {
+		result, err := keysIterator.Next()
+		if err != nil {
+			// Errors print the current listing prior to the error for debug purposes
+			jsonOutput = append(jsonOutput, fmt.Sprintf("keys operation failed. Error accessing state: %s", err))
+			return shim.Error(strings.Join(jsonOutput, "\n"))
+		}
+		_, keyComponents, err := stub.SplitCompositeKey(result.Key)
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+		currentCustomerRecord, err = utils.GetCustomerRecord(stub, keyComponents[1])
+		if err != nil {
+			// Errors print the current listing prior to the error for debug purposes
+			jsonOutput = append(jsonOutput, fmt.Sprintf("keys operation failed. Error accessing Bank Account: %s", err))
+			return shim.Error(strings.Join(jsonOutput, "\n"))
+		}
+		msg := fmt.Sprintf(
+			"Customer ID: %s \n" +
+				"\tAppDevId: %s\n" +
+				"\tBankAccountId: %s\n" +
+				"\tSubscriptionFee: %0.2f\n" +
+				"\tSubscriptionDueDate: %s\n" +
+				"\tQueuedSong: %s\n" +
+				"\tPreviousSong: %s",
+			currentCustomerRecord.Id,
+			currentCustomerRecord.AppDevId,
+			currentCustomerRecord.BankAccountId,
+			currentCustomerRecord.SubscriptionFee,
+			currentCustomerRecord.SubscriptionDueDate.String(),
+			currentCustomerRecord.QueuedSong,
+			currentCustomerRecord.PreviousSong)
+		
+		if appDevId == currentCustomerRecord.AppDevId {
+			jsonOutput = append(jsonOutput, msg)
+		}
+
+		
+	}
+	resultMsg := strings.Join(jsonOutput, "\n")
+	return shim.Success([]byte(resultMsg))
+}
+
